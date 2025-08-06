6432c5425ceb3b3ff32c97f68ab1cc7b526ae581
sumu9897 <sumon.bncd@gmail.com>
2025-08-06 17:01:11 +0600

    Database connected

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
 
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB();
 
 // midddlewares
 app.use(express.json())
