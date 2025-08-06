166c1c8d2dcdf277f0d7b0a20bfe7627994dd591
sumu9897 <sumon.bncd@gmail.com>
2025-08-06 22:23:35 +0600

    initialize Cloudinary config and API setup

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
+import connectCloudinary from './config/cloudinary.js'
 
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB();
+connectCloudinary()
 
 // midddlewares
 app.use(express.json())
