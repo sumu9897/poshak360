d7cc4cc28b3cf67c7a44d317acc475257c88bfbf
sumu9897 <sumon.bncd@gmail.com>
2025-08-08 09:48:27 +0600

    implement admin login and logout functionality

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import AddProduct from "./pages/AddProduct";
+import AllProduct from "./pages/AllProduct";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+
+export const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 const App = () => {
-  return (
-    <div className='bg-gray-50 min-h-screen'>
-      <>
-      <Navbar/>
-      <hr />
-      <div className='flex w-full'>
-        <Sidebar/>
-      </div>
-      </>
-    </div>
-  )
-}
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
 
-export default App
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="add" element={<AddProduct  token={token}/>} />
+                <Route path="list" element={<AllProduct  token={token}/>} />
+                <Route path="orders" element={<Orders  token={token}/>} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
